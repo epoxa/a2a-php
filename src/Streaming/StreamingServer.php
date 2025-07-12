8ee355d32cc6178f70fefb9e39197d282f9908a5
Andrei Besleaga <andrei@LAPTOP-9JJ14NEU.localdomain>
2025-07-12 13:29:14 +0300

    a2a-php - change: fixes

--- src/Streaming/StreamingServer.php
+++ src/Streaming/StreamingServer.php
@@ -32,21 +32,20 @@ class StreamingServer
         try {
             $parsedRequest = $this->jsonRpc->parseRequest($request);
             $message = Message::fromArray($parsedRequest['params']['message']);
-            
+
             $taskId = $message->getTaskId() ?? \Ramsey\Uuid\Uuid::uuid4()->toString();
             $contextId = $message->getContextId() ?? \Ramsey\Uuid\Uuid::uuid4()->toString();
 
-            $context = new RequestContext($taskId, $contextId, $message);
+            $context = new RequestContext($message, $taskId, $contextId);
 
             // Subscribe to events for this task
-            $eventBus->subscribe($taskId, function($event) use ($parsedRequest) {
+            $eventBus->subscribe($taskId, function ($event) use ($parsedRequest) {
                 $response = $this->jsonRpc->createResponse($parsedRequest['id'], $event);
                 $this->streamer->sendEvent(json_encode($response));
             });
 
             // Execute the task
             $executor->execute($context, $eventBus);
-
         } catch (\Exception $e) {
             $error = $this->jsonRpc->createError($request['id'] ?? null, $e->getMessage());
             $this->streamer->sendEvent(json_encode($error));
